cd4052a760f34dab605393ca567133347d3b603d
Iago Casas <137528263+S007mason@users.noreply.github.com>
2026-05-12 15:04:52 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+function decrement() {
+  count.value--
+}
+</script>
+
+<template>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
+  <p>{{ count }}</p>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.card {
+  padding: 2em;
+}
+</style>
